7df3c2fe4615fb7a6fd2756327d5ff6230f07763
niqooll <niqozr01@gmail.com>
2024-07-21 21:18:03 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      dark: "#1e1b4b",
+    },
+  },
+  plugins: [],
+};
